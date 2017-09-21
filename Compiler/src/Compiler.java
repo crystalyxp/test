77f39378c28fc5915eca0b33d867ecab9ea6df58
crystalyxp <y_xp168@163.com>
2017-09-21 12:40:52 +0800

    编译原理

---
+++ Compiler/src/Compiler.java
@@ -0,0 +1,12 @@
+
+public class Compiler {
+
+	public static void main(String[] args) {
+
+
+		Lexer lexer = new Lexer();
+		lexer.runLexer();
+		BasicParser basic_parser = new BasicParser(lexer);
+		basic_parser.statements();
+	}
+}
